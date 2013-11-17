2fcacffdbc187f89652add5ad3bf730dcc1907ef
Frank Singel <fjs52@case.edu>
2013-11-17 18:45:22 -0500

    Functional filters for min, max, avg. Restructuring filter memory length.

--- basic_tests.py
+++ basic_tests.py
@@ -15,9 +15,6 @@ from testify import *
 import filters
 
 #Constants for checking error handling
-NO_MATCH = "Data is empty or does not match. Exiting.\n"
-TOKEN_ERROR = "Error in token: "
-NONSEVEN_LIST_ERROR = "List not of appropriate length\n"
 EXIT_SUCCESS = True
 EXIT_FAILURE = False
 
@@ -25,17 +22,67 @@ class BasisTests(TestCase):
     """
     Tests all conditionals
     """
-    pass
+    def test_max(self):
+        max_filter = filters.MaxFilter(3)
+        max_filter.add(1)
+        assert_equals([1,1], max_filter.evaluate())
+        max_filter.add(2)
+        assert_equals([2,2], max_filter.evaluate())
+        max_filter.add(3)
+        assert_equals([3,3], max_filter.evaluate())
+        max_filter.add(1)
+        assert_equals([3,3], max_filter.evaluate())
+        max_filter.add(1)
+        assert_equals([3,3], max_filter.evaluate())
+        max_filter.add(1)
+        assert_equals([1,3], max_filter.evaluate())
+        max_filter.add(4)
+        assert_equals([4,4], max_filter.evaluate())
+        max_filter.reset()
+        assert_equals([0,0], max_filter.evaluate())
+
+    def test_min(self):
+        min_filter = filters.MinFilter(3)
+        min_filter.add(1)
+        assert_equals([1,1], min_filter.evaluate())
+        min_filter.add(2)
+        assert_equals([1,1], min_filter.evaluate())
+        min_filter.add(3)
+        assert_equals([1,1], min_filter.evaluate())
+        min_filter.add(3)
+        assert_equals([2,1], min_filter.evaluate())
+        min_filter.add(3)
+        assert_equals([3,1], min_filter.evaluate())
+        min_filter.add(1)
+        assert_equals([1,1], min_filter.evaluate())
+        min_filter.add(0)
+        assert_equals([0,0], min_filter.evaluate())
+        min_filter.reset()
+        min_filter.add(1)
+        assert_equals([1,1], min_filter.evaluate())
+
+    def test_avg(self):
+        avg_filter = filters.AvgFilter(3)
+        avg_filter.add(1)
+        assert_equals([1,1], avg_filter.evaluate())
+        avg_filter.add(2)
+        assert_equals([1.5,1.5], avg_filter.evaluate())
+        avg_filter.add(3)
+        assert_equals([2,2], avg_filter.evaluate())
+        avg_filter.add(3)
+        assert_equals([8/3,3], avg_filter.evaluate())
+        avg_filter.reset()
+        assert_equals([0,0], avg_filter.evaluate())
 
 class BoundaryTests(TestCase):
     """
     Test for off-by-one errors: just above/below/on min
     Contains most of BadData tests
     """
-    def test_min_input(self, output):
+    def test_min_input(self):
         pass
 
-    def test_no_number(self, output):
+    def test_no_number(self):
         pass
 
 class DataFlow(TestCase):
@@ -46,14 +93,14 @@ class DataFlow(TestCase):
     def setUp(self):
         pass
 
-    def test_multiple_lines(self, output):
+    def test_multiple_lines(self):
         pass
 
 class BadData(TestCase):
     """
     Test some bad data
     """
-    def test_bad_number(self, output):
+    def test_bad_number(self):
         pass
 
 class StressTest(TestCase):
@@ -62,7 +109,7 @@ class StressTest(TestCase):
     """
 
     @suite('stress', reason="Time Intensive Stress Test not needed on every test run")
-    def test_more_input(self, output):
+    def test_more_input(self):
         pass
 
 if __name__ == '__main__':
--- filters.py
+++ filters.py
@@ -8,54 +8,73 @@ This module manages the creation and function of filters.
 
 """
 
+from __future__ import division
 import copy
 import sys 
+
+LIMITLESS = -1;
 
 class Filter(object):
     """
     Parent class for other filters
     """
-
     def __init__(self, size):
         """
         Constructor............................
         """
         self.memory_size = size
-        self.short = []
-        self.long = []
+        self.short_term = []
+        self.long_term = []
         print "Initted with size of: " + str(self.memory_size)
         
     def reset(self):
         """
         Resets the values previous input so far
         """
-        #return values first
-        self.short = []
-        self.long = []
+        #store return values first
+        self.short_term = []
+        self.long_term = []
         print "Reset"
 
     def add(self, value):
         """
         Adds a value to the filter
         """
-        if len(self.short) >= self.memory_size:
-            print "Forgot" + str(short[0])
-            self.short = self.short[1:]
-
-        self.short.append(value)
-        self.long.append(value)
+        self.short_term.append(value)
+        self.long_term.append(value)
         print ("Added" + str(value))
 
+        if len(self.short_term) > self.memory_size:
+            print "Forgot" + str(self.short_term[0])
+            self.short_term = self.short_term[1:]
+
+    def evaluate(self):
+        raise NotImplementedError("This needs implemented")
+
 class MaxFilter(Filter):
-    pass
+    def evaluate(self):
+        if len(self.short_term) == 0:
+            return [0,0]
+        return [max(self.short_term), max(self.long_term)]
 
 class MinFilter(Filter):
-    pass
+    def evaluate(self):
+        if len(self.short_term) == 0:
+            return [0,0]
+        return [min(self.short_term), min(self.long_term)]
 
 class AvgFilter(Filter):
-    pass
+    def evaluate(self):
+        if len(self.short_term) == 0:
+            return [0,0]
+        print self.short_term
+        print sum(self.short_term)
+        print len(self.short_term)
+        print sum(self.short_term)/len(self.short_term)
+        print self.long_term
+        return [sum(self.short_term)/len(self.short_term),
+                sum(self.long_term)/len(self.short_term)]
 
 class CascadeFilter(Filter):
-
     def __init__(self, first_filter, second_filter):
         pass
