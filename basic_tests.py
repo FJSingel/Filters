35b47abc7312043fb93586073d1fb736c8f589da
Frank Singel <fjs52@case.edu>
2013-12-01 23:58:44 -0500

    Updated comments in basic_tests.py

--- basic_tests.py
+++ basic_tests.py
@@ -1,5 +1,8 @@
 """
-Unit tests for filters
+Created on Nov 14, 2013
+Last updated Dec 1, 2013
+
+Unit tests for filters.py
 Frank Singel
 """
 
@@ -8,18 +11,15 @@ from testify import *
 import filters
 
 class FilterTests(TestCase):
-    '''
-    Tests each general filter
-    '''
+    """Tests each general filter"""
     def test_filter(self):
+        """Ensure basic filter doesn't work"""
         filt = filters.Filter(3)
         with assert_raises(NotImplementedError):
             filt.add(5)
 
     def test_max_limited(self):
-        '''
-        Add and confirm 1,2,3,1,1,1,4,reset
-        '''
+        """Add and confirm input of: 1,2,3,1,1,1,4,reset"""
         max_filter = filters.MaxFilter(3)
         max_filter.add(1)
         max_filter.add(2)
@@ -35,9 +35,7 @@ class FilterTests(TestCase):
 
 
     def test_min_limited(self):
-        '''
-        Add and confirm 1,2,3,3,3,1,0,reset
-        '''
+        """Add and confirm input of: 1,2,3,3,3,1,0,reset"""
         min_filter = filters.MinFilter(3)
         min_filter.add(1)
         min_filter.add(2)
@@ -51,9 +49,7 @@ class FilterTests(TestCase):
         assert_equals([], min_filter.outputs)
 
     def test_avg_limited(self):
-        '''
-        Add and confirm 3,6,9,9,reset
-        '''
+        """Add and confirm input of: 3,6,9,9,reset"""
         avg_filter = filters.AvgFilter(3)
         avg_filter.add(3)
         avg_filter.add(6)
@@ -64,9 +60,7 @@ class FilterTests(TestCase):
         assert_equals([], avg_filter.outputs)
 
     def test_max_unlimited(self):
-        '''
-        Add and confirm 1,2,3,1,1,1,4,reset
-        '''
+        """Add and confirm input of: 1,2,3,1,1,1,4,reset"""
         max_filter = filters.MaxFilter(filters.LIMITLESS)
         max_filter.add(1)
         max_filter.add(2)
@@ -79,9 +73,7 @@ class FilterTests(TestCase):
         assert_equals([], max_filter.outputs)
 
     def test_min_unlimited(self):
-        '''
-        Add and confirm 1,2,3,3,3,1,0,reset,1
-        '''
+        """Add and confirm input of: 1,2,3,3,3,1,0,reset,1"""
         min_filter = filters.MinFilter(filters.LIMITLESS)
         min_filter.add(1)
         min_filter.add(2)
@@ -96,9 +88,7 @@ class FilterTests(TestCase):
         assert_equals([1], min_filter.outputs)
 
     def test_avg_unlimited(self):
-        '''
-        Add and confirm 1,2,3,3,-9,reset
-        '''
+        """Add and confirm input of: 1,2,3,3,-9,reset"""
         avg_filter = filters.AvgFilter(filters.LIMITLESS)
         avg_filter.add(1)
         avg_filter.add(2)
@@ -110,9 +100,7 @@ class FilterTests(TestCase):
         assert_equals([], avg_filter.outputs)
 
     def test_cascade(self):
-        '''
-        Feed Max2 into Min3 using values from pdf
-        '''
+        """Feed a Max2 filter into a Min3 one using values from pdf"""
         min_filter = filters.MinFilter(3)
         max_filter = filters.MaxFilter(2)
         cascade = filters.CascadeFilter(max_filter, min_filter)
@@ -125,9 +113,7 @@ class FilterTests(TestCase):
         assert_equals([-1,-1,-1,2,2], cascade.outputs)
 
     def test_scalar_linear(self):
-        '''
-        Test input from project description
-        '''
+        """Test input from project description"""
         outs = [.1, .1, .1, .1, .1]
         ins = [.5, .5, .5, .5, .5]
         scalar = filters.ScalarLinearFilter(ins, outs)
@@ -150,9 +136,7 @@ class FilterTests(TestCase):
             scalar2 = filters.ScalarLinearFilter(ins, outs[1:])
 
     def test_FIR(self):
-        '''
-        Test input of 1,2,3,reset,5 for a filter of gain = 3
-        '''
+        """Test input of 1,2,3,reset,5 for a filter of gain = 3"""
         fir = filters.FIRFilter(3)
         fir.add(1)
         fir.add(2)
@@ -163,9 +147,7 @@ class FilterTests(TestCase):
         assert_equals([15], fir.outputs)
 
     def test_binomial(self):
-        '''
-        Test the growth of pascal's triangle
-        '''
+        """Test the growth of pascal's triangle"""
         binom = filters.BinomialFilter()
         binom.add(1)
         binom.add(1)
@@ -175,10 +157,9 @@ class FilterTests(TestCase):
         assert_equals([1,4,6,4,1], binom.gain)
         binom.add(1)
         assert_equals([1,5,10,10,5,1], binom.gain)
-        print binom.outputs
-
+        
     def test_eqs(self):
-        '''Tests filter equality'''
+        """Tests filter equality"""
         min_filter1 = filters.MinFilter(3)
         min_filter2 = filters.MinFilter(3)
         min_filter3 = filters.MinFilter(2)
